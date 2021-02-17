33082bbfa0b9804339c74ab5e9b1d687cb212441
AndoresuSama <aaeg30066@gmail.com>
2021-02-17 13:14:45 -0300

    Implementado modo oscuro, queda pendiente añadir funcionalidad con local storage para que vuelva a modo light cuando se desee

--- assets/js/clase9.js
+++ assets/js/clase9.js
@@ -1,15 +1,15 @@
-document.getElementById("darkModeButton").addEventListener("click",enableDarkMode)
-
+let findButton = document.getElementById("darkModeButton");
 let findNav = document.getElementById("mainNav");
 let findRqstForm = document.getElementById("section-request-form");
+let findBody = document.getElementById("body-modiffier");
 let findFooter = document.getElementById("footer");
 let findLogo = document.getElementsByTagName("img");
 
-if(document.getElementById("darkModeButton").value == "Click to enable Dark Mode"){
-    enableDarkMode()
+if(findButton.value == "Click to enable Dark Mode"){
+    findButton.addEventListener("click",enableDarkMode);
 }
 if(document.getElementById("darkModeButton").value == "Click to enable Light Mode"){
-    disableDarkMode()
+    findButton.addEventListener("click",disableDarkMode);
 }
 
 function enableDarkMode(){
@@ -17,16 +17,18 @@ function enableDarkMode(){
     findNav.setAttribute("class", "navbar navbar-expand-lg darkModeEnabled text-uppercase fixed-top");
     findRqstForm.setAttribute("class", "darkModeEnabled");
     findFooter.setAttribute("class", "darkModeEnabled");
+    findBody.setAttribute("class", "darkModeEnabled");
     findLogo[0].removeAttribute("src");
-    findLogo[0].setAttribute("src", "./assets/img/logo_lettering_Blanco_2.png")
+    findLogo[0].setAttribute("src", "./assets/img/logo_lettering_Blanco_2.png");
     darkModeButton.value = "Click to enable Light Mode"
 }
 
 function disableDarkMode(){
     findNav.removeAttribute("class");
     findNav.setAttribute("class", "navbar navbar-expand-lg bg-secondary text-uppercase fixed-top");
-    findRqstForm.removeAttribute("class")
-    findFooter.removeAttribute("class")
+    findRqstForm.removeAttribute("class");
+    findFooter.removeAttribute("class");
+    findBody.removeAttribute("class");
     findLogo[0].removeAttribute("src");
-    findLogo[0].setAttribute("src", "./assets/img/logo lettering Negro.png")
+    findLogo[0].setAttribute("src", "./assets/img/logo lettering Negro.png");
 }
