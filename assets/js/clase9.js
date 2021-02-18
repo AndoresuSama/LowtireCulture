1b22dce349f5781c171d24cc6a8ccf3901d0a715
AndoresuSama <aaeg30066@gmail.com>
2021-02-17 21:01:59 -0300

    Completado boton de modo oscuro

--- assets/js/clase9.js
+++ assets/js/clase9.js
@@ -4,15 +4,32 @@ let findRqstForm = document.getElementById("section-request-form");
 let findBody = document.getElementById("body-modiffier");
 let findFooter = document.getElementById("footer");
 let findLogo = document.getElementsByTagName("img");
+let getDarkModeStorage = localStorage.getItem("Dark mode");
 
-if(findButton.value == "Click to enable Dark Mode"){
-    findButton.addEventListener("click",enableDarkMode);
+findButton.addEventListener("click", enableDarkMode);
+
+let tema = localStorage.getItem("Dark mode");
+darkModeSwitch(tema);
+
+function darkModeSwitch(tema){
+    if(tema == "on"){
+        enableDarkMode();
+    }else if(tema == "off"){
+        disableDarkMode();
+    }else{
+        enableDarkMode();
+        disableDarkMode();
+    }
 }
-if(document.getElementById("darkModeButton").value == "Click to enable Light Mode"){
-    findButton.addEventListener("click",disableDarkMode);
-}
+
+/*function darkModeSwitch(tema){
+    if(tema == off){
+        findButton.addEventListener("click", enableDarkMode)
+    }
+}*/
 
 function enableDarkMode(){
+    localStorage.setItem("Dark mode", "on");
     findNav.removeAttribute("class");
     findNav.setAttribute("class", "navbar navbar-expand-lg darkModeEnabled text-uppercase fixed-top");
     findRqstForm.setAttribute("class", "darkModeEnabled");
@@ -20,10 +37,13 @@ function enableDarkMode(){
     findBody.setAttribute("class", "darkModeEnabled");
     findLogo[0].removeAttribute("src");
     findLogo[0].setAttribute("src", "./assets/img/logo_lettering_Blanco_2.png");
-    darkModeButton.value = "Click to enable Light Mode"
+    darkModeButton.value = "Click to enable Light Mode";
+    findButton.removeEventListener("click", enableDarkMode);
+    findButton.addEventListener("click", disableDarkMode);
 }
 
 function disableDarkMode(){
+    localStorage.setItem("Dark mode", "off");
     findNav.removeAttribute("class");
     findNav.setAttribute("class", "navbar navbar-expand-lg bg-secondary text-uppercase fixed-top");
     findRqstForm.removeAttribute("class");
@@ -31,4 +51,6 @@ function disableDarkMode(){
     findBody.removeAttribute("class");
     findLogo[0].removeAttribute("src");
     findLogo[0].setAttribute("src", "./assets/img/logo lettering Negro.png");
+    findButton.removeEventListener("click", disableDarkMode);
+    findButton.addEventListener("click", enableDarkMode);
 }
